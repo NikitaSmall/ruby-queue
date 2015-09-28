43e845a925b451204b63a97277f233d32ef058bf
nikitasmall <nikitasosnov@yahoo.com>
2015-09-28 13:40:52 +0300

    cleanup

--- lib/handlers/google_analytics/user.rb
+++ lib/handlers/google_analytics/user.rb
@@ -12,31 +12,6 @@ module Handlers
 
         @api = ApiFactory.new.analytics_api(self)
       end
-
-      def webproperties
-        @webproperties ||= get_webproperties
-      end
-
-      def profiles(webproperties = nil)
-        @profiles ||= get_profiles(webproperties)
-      end
-
-      def update_darkwing
-        @darkwing_user.update(
-          analytics_access_token: self.analytics_access_token,
-          analytics_refresh_token: self.analytics_refresh_token,
-          analytics_token_issued_at: self.analytics_token_issued_at
-        )
-      end
-
-      private
-      def get_webproperties
-        GoogleAnalytics::AnalyticsWebsitesReport.new(@api).fetch_webproperties
-      end
-
-      def get_profiles(webproperties)
-        GoogleAnalytics::AnalyticsWebsitesReport.new(@api).fetch_profiles(webproperties)
-      end
     end
   end
 end
